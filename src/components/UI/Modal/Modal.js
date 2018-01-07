e5a49869f60fefc115b8152aad0da0f017aac182
Roman <rm07ru@gmail.com>
2018-01-08 00:13:33 +0300

    Add Backdrop to Modal

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,17 +1,22 @@
 import React from 'react';
 
+import Aux from '../../../hoc/Auxiliary/Auxiliary';
+import Backdrop from '../Backdrop/Backdrop';
 import styles from './Modal.css';
 
 const Modal = (props) => {
   return (
-    <div
-      style={{
-        display: props.show ? 'block' : 'none'
-      }}
-      className={styles.Modal}
-    >
-      {props.children}
-    </div>
+    <Aux>
+      <div
+        style={{
+          display: props.show ? 'block' : 'none'
+        }}
+        className={styles.Modal}
+      >
+        {props.children}
+      </div>
+      <Backdrop />
+    </Aux>
   );
 };
 
