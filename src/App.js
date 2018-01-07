645036bb5059eae41ae8622ab0dd110716fba9e3
Roman <rm07ru@gmail.com>
2018-01-07 21:24:46 +0300

    Add react-router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import BoardsList from './containers/BoardsList/BoardsList';
+import Board from './containers/Board/Board';
 
 class App extends Component {
+  componentWillMount() {
+    console.log(this.props)
+  }
+
   render() {
-    return <BoardsList />;
+    return (
+      <Switch>
+        <Route exact path="/" component={BoardsList} />
+        <Route exact path="/board" component={Board} />
+      </Switch>
+    );
   }
 }
 
--- src/containers/BoardsList/BoardsList.js
+++ src/containers/BoardsList/BoardsList.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react';
 
 class BoardsList extends Component {
+  state = {
+    boards: [
+      {
+        id: '1',
+        name: 'Board 1',
+      },
+      {
+        id: '2',
+        name: 'Board 2',
+      }
+    ]
+  }
+
   render() {
     return 'Boards List';
   }
