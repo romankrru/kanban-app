226ef75e72d7ea561bf55b421a618b4b9fcecffd
Roman <rm07ru@gmail.com>
2018-01-15 19:32:01 +0300

    Refactor App component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-/* eslint-disable */
-import React, { Component } from 'react';
+import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import BoardsList from './containers/BoardsList/BoardsList';
 import Board from './containers/Board/Board';
 import Layout from './components/Layout/Layout';
 
-class App extends Component {
-  render() {
-    return (
-      <Layout>
-        <Switch>
-          <Route exact path="/board/:id" component={Board} />
-          <Route path="/" component={BoardsList} />
-        </Switch>
-      </Layout>
-    );
-  }
-}
+const App = () => (
+  <Layout>
+    <Switch>
+      <Route exact path="/board/:id" component={Board} />
+      <Route path="/" component={BoardsList} />
+    </Switch>
+  </Layout>
+);
 
 export default App;
