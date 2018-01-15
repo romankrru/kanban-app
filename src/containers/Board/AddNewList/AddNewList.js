e1890bb063e82f70bfda5ea784702beea29a04f1
Roman <rm07ru@gmail.com>
2018-01-15 19:38:15 +0300

    Add prop types validation

--- src/containers/Board/AddNewList/AddNewList.js
+++ src/containers/Board/AddNewList/AddNewList.js
@@ -1,19 +1,24 @@
-/* eslint-disable */
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import styles from './AddNewList.css';
 
-const AddNewList = (props) => {
-  return (
-    <div className={styles.AddNewListWrapper}>
-      <div 
-        className={styles.AddNewList}
-        onClick={props.onListAdd}
-      >
+const AddNewList = props => (
+  <div className={styles.AddNewListWrapper}>
+    <div
+      className={styles.AddNewList}
+      onClick={props.onListAdd}
+      role="button"
+      onKeyPress={props.onListAdd}
+      tabIndex="0"
+    >
         Add new List
-      </div>
     </div>
-  );
+  </div>
+);
+
+AddNewList.propTypes = {
+  onListAdd: PropTypes.func.isRequired,
 };
 
 export default AddNewList;
