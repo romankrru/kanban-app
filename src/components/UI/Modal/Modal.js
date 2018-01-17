6bc6a7f56953cd4e973c87a28460e223e85ca4bf
Roman <rm07ru@gmail.com>
2018-01-17 19:50:43 +0300

    Add prop types validation to Modal

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,27 +1,37 @@
-/* eslint-disable */
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import Aux from '../../../hoc/Auxiliary/Auxiliary';
 import Backdrop from '../Backdrop/Backdrop';
 import styles from './Modal.css';
 
-const Modal = (props) => {
-  return (
-    <Aux>
-      <div
-        style={{
-          display: props.show ? 'block' : 'none'
+const Modal = props => (
+  <Aux>
+    <div
+      style={{
+          display: props.show ? 'block' : 'none',
         }}
-        className={styles.Modal}
-      >
-        {props.children}
-      </div>
-      <Backdrop 
-        onClick={props.onBackdropClick}
-        show={true}
-      />
-    </Aux>
-  );
+      className={styles.Modal}
+    >
+      {props.children}
+    </div>
+    <Backdrop
+      onClick={props.onBackdropClick}
+      show
+    />
+  </Aux>
+);
+
+Modal.defaultProps = {
+  show: false,
+  children: null,
+  onBackdropClick: () => {},
+};
+
+Modal.propTypes = {
+  show: PropTypes.bool,
+  children: PropTypes.node,
+  onBackdropClick: PropTypes.func,
 };
 
 export default Modal;
